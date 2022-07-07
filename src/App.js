da30f7b86cbd5872024695be5146f8361145c221
aguilanbon <aguilanbon@gmail.com>
2022-07-07 19:42:08 +0800

    increment usereducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useReducer } from 'react';
 import './index.css';
 
 function App() {
+
+
+  // const initial = {
+  //   post: []
+  // }
+
+  // // const url = 'https://random-data-api.com/api/'
+
+  const initial = {
+    count: 0
+  }
+
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'increment':
+        return { count: state.count + 1 }
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initial)
+
   return (
     <div className="App">
-      <h1 className='text-red-500' >Hello again</h1>
+      <button>decrement</button>
+      <h1 className='text-red-500' >{state.count}</h1>
+      <button onClick={() => dispatch({type: 'increment'})}>increment</button>
     </div>
   );
 }
