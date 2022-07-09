258731d2c91cf25673f2b5e09aac4259de08630a
aguilanbon <aguilanbon@gmail.com>
2022-07-09 10:21:37 +0800

    todo using useReducer

--- src/App.js
+++ src/App.js
@@ -1,64 +1,42 @@
+import { useState } from 'react';
 import { useReducer } from 'react';
 import './index.css';
 
 function App() {
 
-  // const initial = {
-  //   count: 0
-  // }
-
-  // const reducer = (state, action) => {
-  //   switch (action.type) {
-  //     case 'increment':
-  //       return { count: state.count + 1 }
-  //     case 'decrement':
-  //       return { count: state.count - 1 }
-  //     case 'reset': 
-  //       return { count: initial.count }
-
-  //     default:
-  //       return state
-  //   }
-  // }
-
-  const initial = {
-    name : '',
-    password : '',
-    email : ''
-  }
+  const [todo, setTodo] = useState('')
 
   const reducer = (state, action) => {
     switch (action.type) {
-      case 'name':
-        return {...state, name: action.payload.name}
-      case 'password':
-        return {...state, password: action.payload.password}
-      case 'email':
-        return {...state, email: action.payload.email}
+      case 'ADD_TODO':
+        return [...state, action.payload]
       default:
         return { state }
     }
   }
 
   const handleSubmit = () => {
-    alert(state.name + state.password + state.email)
+    dispatch({ type: 'ADD_TODO', payload: {name: todo, id: Date.now(), isCompleted: false} })
+    setTodo('')
   }
 
-  const [state, dispatch] = useReducer(reducer, initial)
+  const [state, dispatch] = useReducer(reducer, [])
 
+  console.log(state)
   return (
     <div className="w-screen">
-      {/* <button onClick={() => dispatch({ type: 'decrement' }) }>decrement</button>
-      <h1 className='text-red-500' >{state.count}</h1>
-      <button onClick={() => dispatch({type: 'increment'})}>increment</button>
-      <button onClick={() => dispatch({type: 'reset'}) } >Reset</button> */}
-
       <div className='flex flex-col w-full items-center justify-center'>
         <div className='w-96 flex flex-col'>
-          <input className='border-b-2 border-green-400 focus:outline-none  p-1' type="text" name='name' placeholder='name' onChange={(e) => dispatch({ type: 'name', payload: {name: e.target.value} })} />
-          <input className='border-b-2 border-green-400 focus:outline-none  p-1' type="email" name='email' placeholder='email' onChange={(e) => dispatch({ type: 'email', payload: {email: e.target.value} })} />
-          <input className='border-b-2 border-green-400 focus:outline-none  p-1' type="password" name='password' placeholder='password' onChange={(e) => dispatch({ type: 'password', payload: {password: e.target.value} })} />
-          <button onClick={() => handleSubmit() }>Submit</button>
+          <input className='border-b-2 border-green-400 focus:outline-none  p-1' type="text" name='name' placeholder='name' value={todo}  onChange={(e) => setTodo(e.target.value)} />
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+        <div className='w-96 flex flex-col m-8'>
+            <h1>Your Todos</h1>
+            {state.map(userTodo => (
+              <div key={userTodo.id}>
+                <p>{userTodo.name}</p>
+              </div>
+            ))}
         </div>
       </div>
     </div>
